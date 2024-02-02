73531b4921fea7b6d5ef0679907a63f540875477
Nilesh1409 <nileshtiwari7054@gmail.com>
2024-02-02 15:13:31 +0530

    Added video call feature

--- src/components/Chat/ChatWindow.jsx
+++ src/components/Chat/ChatWindow.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
+import SimplePeer from "simple-peer";
+
 import {
   TextField,
   Button,
@@ -8,7 +10,11 @@ import {
   ListItem,
   ListItemText,
   Paper,
+  AppBar,
+  Toolbar,
+  Typography,
 } from "@material-ui/core";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
 import { IconButton } from "@material-ui/core";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
 const useStyles = makeStyles((theme) => ({
@@ -16,13 +22,34 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     height: "80vh",
-    maxWidth: "75%",
-    minWidth : "75%",
+    maxWidth: "100%",
+    minWidth: "100%",
     margin: "auto",
     border: `1px solid ${theme.palette.divider}`,
     borderRadius: theme.shape.borderRadius,
     overflow: "hidden",
     backgroundColor: "#E5DDD5",
+    // border: "1px solid red",
+  },
+  chatContainer: {
+    display: "flex",
+    flexDirection: "column",
+    height: "80vh",
+    maxWidth: "75%",
+    minWidth: "75%",
+    margin: "auto",
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+    overflow: "hidden",
+    backgroundColor: "#E5DDD5",
+    position: "relative",
+  },
+  chatNavbar: {
+    position: "absolute",
+    display: "flex",
+    flexDirection: "row-reverse",
+    paddingRight: "10px",
+    backgroundColor: "#128C7E",
   },
   messageArea: {
     flexGrow: 1,
@@ -74,6 +101,11 @@ const ChatWindow = (props) => {
     publicKey,
     myPublicKey,
   } = props;
+  const [stream, setStream] = useState(null);
+  const videoRef = useRef(null);
+  const [peers, setPeers] = useState([]);
+  const peersRef = useRef([]);
+  const [videoStarted, setVideoStarted] = useState(false);
 
   const fetchChat = async () => {
     try {
@@ -95,7 +127,6 @@ const ChatWindow = (props) => {
     const file = event.target.files[0];
     console.log("🚀 ~ handleImageChange ~ file:", file);
     if (file) {
-
       // Prepare to send this file
       const formData = new FormData();
       formData.append("image", file);
@@ -124,7 +155,7 @@ const ChatWindow = (props) => {
             sender: userId,
             receiver: friendId,
           };
-        //   console.log("🚀 ~ handleImageChange ~ imageMessage:", imageMessage);
+          //   console.log("🚀 ~ handleImageChange ~ imageMessage:", imageMessage);
           socket.emit("chat message", {
             chatRoom: room,
             encryptedMessage: imageMessage,
@@ -138,67 +169,197 @@ const ChatWindow = (props) => {
     }
   };
 
+  const enableStream = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      console.log("stream", stream);
+      setStream(stream);
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    } catch (err) {
+      // Handle the error
+      console.error("Failed to get user media", err);
+    }
+  };
+  const createPeerConnection = (userToSignal, callerID, stream) => {
+    const peer = new SimplePeer({
+      initiator: true,
+      trickle: false,
+      stream,
+    });
+
+    peer.on("signal", (signal) => {
+      // Signal the peer who you want to call
+      console.log(
+        "🚀 ~ peer.on ~ userToSignal, callerID, signal:",
+        userToSignal
+      );
+      console.log("🚀 ~ peer.on ~ signal:", signal);
+      console.log("🚀 ~ peer.on ~ callerID:", callerID);
+      socket.emit("sending signal", { userToSignal, callerID, signal });
+    });
+
+    return peer;
+  };
+
+  //  To start call
+
+  const startCall = () => {
+    setVideoStarted(true);
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((stream) => {
+        setStream(stream);
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+
+        // Directly create a peer connection for the user you want to call
+        const peer = createPeerConnection(friendId, socket.id, stream);
+        peersRef.current.push({
+          peerID: friendId,
+          peer,
+        });
+        setPeers([peer]);
+      });
+  };
+
+  // For incoming call
+  const addPeer = (incomingSignal, callerID, stream) => {
+    const peer = new SimplePeer({
+      initiator: false,
+      trickle: false,
+      stream,
+    });
+
+    peer.on("signal", (signal) => {
+      socket.emit("returning signal", { signal, callerID });
+    });
+
+    peer.signal(incomingSignal);
+
+    return peer;
+  };
+
   useEffect(() => {
     // setChat([]);
     // fetchChat();
+    // stream?.getTracks().forEach((track) => track.stop());
+    socket.on("user joined", (payload) => {
+      const peer = addPeer(payload.signal, payload.callerID, stream);
+      peersRef.current.push({
+        peerID: payload.callerID,
+        peer,
+      });
+
+      setPeers((users) => [...users, peer]);
+    });
+
+    socket.on("receiving returned signal", (payload) => {
+      const item = peersRef.current.find((p) => p.peerID === payload.id);
+      item.peer.signal(payload.signal);
+    });
   }, []);
+  useEffect(() => {
+    console.log("Setting up socket event listeners");
+    socket.on("incoming call", ({ from, signal }) => {
+      console.log("Incoming call from:", from);
+      console.log("Signal data:", signal);
+      // rest of your code...
+    });
+
+    return () => {
+      console.log("Cleaning up socket event listeners");
+      // socket.off("incoming call");
+    };
+  }, [socket]);
+  // useEffect(() => {
+  //   socket.on("connect", () => {
+  //     console.log(`Connected to server with socket ID: ${socket.id}`);
+  //   });
+
+  //   // ...
+  // }, [socket]);
 
   return (
-    <Paper className={classes.chatWindow}>
-      <List className={classes.messageArea}>
-        {chat?.map((msg, index) => (
-          <ListItem
-            key={index}
-            className={`${classes.messageBubble} ${
-              msg.sender === userId
-                ? classes.senderBubble
-                : classes.receiverBubble
-            }`}
-          >
-            <ListItemText
-              primary={decryptMessage(
-                msg.message,
-                msg.sender === userId ? publicKey : publicKey
-              )}
-            />
-            {msg.image ? (
-              <img
-                src={`${process.env.REACT_APP_BASE_URL}/${msg.image}`}
-                alt="Uploaded"
-                style={{ maxWidth: "100%", height: "auto" }}
+    <div className={classes.chatContainer}>
+      {videoStarted ? (
+        <video ref={videoRef} autoPlay playsInline />
+      ) : (
+        <>
+          <AppBar className={classes.chatNavbar}>
+            <Toolbar>
+              <Typography
+                onClick={() => startCall()}
+                variant="h6"
+                component="div"
+              >
+                <VideoCallIcon />
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Paper className={classes.chatWindow}>
+            <List className={classes.messageArea}>
+              {chat?.map((msg, index) => (
+                <ListItem
+                  key={index}
+                  className={`${classes.messageBubble} ${
+                    msg.sender === userId
+                      ? classes.senderBubble
+                      : classes.receiverBubble
+                  }`}
+                >
+                  <ListItemText
+                    primary={decryptMessage(
+                      msg.message,
+                      msg.sender === userId ? publicKey : publicKey
+                    )}
+                  />
+                  {msg.image ? (
+                    <img
+                      src={`${process.env.REACT_APP_BASE_URL}/${msg.image}`}
+                      alt="Uploaded"
+                      style={{ maxWidth: "100%", height: "auto" }}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </ListItem>
+              ))}
+            </List>
+            <form className={classes.messageInput} onSubmit={sendMessage}>
+              <input
+                accept="image/*"
+                style={{ display: "none" }}
+                id="icon-button-file"
+                type="file"
+                onChange={handleImageChange}
               />
-            ) : (
-              ""
-            )}
-          </ListItem>
-        ))}
-      </List>
-      <form className={classes.messageInput} onSubmit={sendMessage}>
-        <input
-          accept="image/*"
-          style={{ display: "none" }}
-          id="icon-button-file"
-          type="file"
-          onChange={handleImageChange}
-        />
-        <label htmlFor="icon-button-file">
-          <IconButton color="primary" component="span">
-            <PhotoCamera />
-          </IconButton>
-        </label>
-        <TextField
-          label="Type a message"
-          variant="outlined"
-          size="small"
-          className={classes.inputField}
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <Button variant="contained" color="primary" type="submit">
-          Send
-        </Button>
-      </form>
-    </Paper>
+              <label htmlFor="icon-button-file">
+                <IconButton color="primary" component="span">
+                  <PhotoCamera />
+                </IconButton>
+              </label>
+              <TextField
+                label="Type a message"
+                variant="outlined"
+                size="small"
+                className={classes.inputField}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <Button variant="contained" color="primary" type="submit">
+                Send
+              </Button>
+            </form>
+          </Paper>
+        </>
+      )}
+    </div>
   );
 };
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,43 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate, Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, TextField, Button, Container, Paper, Grid, Link as MuiLink } from '@material-ui/core';
-import nacl from 'tweetnacl';
-import naclUtil from 'tweetnacl-util';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Typography,
+  TextField,
+  Button,
+  Container,
+  Paper,
+  Grid,
+  Link as MuiLink,
+} from "@material-ui/core";
+import nacl from "tweetnacl";
+import naclUtil from "tweetnacl-util";
 const useStyles = makeStyles((theme) => ({
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     marginTop: theme.spacing(2),
     padding: theme.spacing(3),
   },
   form: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
     gap: theme.spacing(2),
-    width: '100%',
+    width: "100%",
   },
 }));
 
 function Login(props) {
   const classes = useStyles();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
-
 
     // const keyPair = nacl.box.keyPair();
     // const publicKey = naclUtil.encodeBase64(keyPair.publicKey);
@@ -39,17 +46,20 @@ function Login(props) {
     // Store the private key in local storage (consider more secure alternatives)
     // localStorage.setItem('privateKey', privateKey);
     try {
-      const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/api/login`, { username, password });
+      const response = await axios.post(
+        `${process.env.REACT_APP_BASE_URL}/api/login`,
+        { username, password }
+      );
       // Store the token for authenticated requests
-      localStorage.setItem('authToken', response.data.token);
-      localStorage.setItem('userId', response.data.userId);
-      localStorage.setItem('publicKey', response.data?.publicKey);
-      localStorage.setItem('userName', response.data?.username);
+      localStorage.setItem("authToken", response.data.token);
+      localStorage.setItem("userId", response.data.userId);
+      localStorage.setItem("publicKey", response.data?.publicKey);
+      localStorage.setItem("userName", response.data?.username);
       // At this point, the private key should already be in local storage from registration
-      console.log('Logged in successfully');
+      console.log("Logged in successfully");
       // Redirect to chat or another appropriate component
       props.setIsAuthenticated(true);
-      localStorage.setItem('isLoggedIn', true);
+      localStorage.setItem("isLoggedIn", true);
       navigate("/chat");
     } catch (error) {
       console.error("Login error", error.response);
@@ -80,12 +90,7 @@ function Login(props) {
             fullWidth
             required
           />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-          >
+          <Button type="submit" variant="contained" color="primary" fullWidth>
             Login
           </Button>
         </form>
